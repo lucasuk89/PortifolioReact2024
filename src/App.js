6ef491e23f03c3ced90f22f07a8d76090f052c6c
Lucas Ferraz Martins <lucasuk89@gmail.com>
2024-04-30 10:36:51 +0200

    Created Data folder with json and completed navbar css

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
       </Router>
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -36,6 +36,89 @@ function Navbar() {
       <div>
         <img src="./img/logo.svg" alt="Logoipsum" />
       </div>
+      <a
+        className={`nav__hamburguer ${navActive ? "active" : ""}`}
+        onClick={toggleNav}
+      >
+        <span className="nav__hamburguer__line"></span>
+        <span className="nav__hamburguer__line"></span>
+        <span className="nav__hamburguer__line"></span>
+      </a>
+      <div className={`navbar--items ${navActive ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="heroSection"
+              className="navbar-content"
+            >
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="MyPortfolio"
+              className="navbar-content"
+            >
+              Portfolio
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="AboutMe"
+              className="navbar-content"
+            >
+              About Me
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="Testimonials"
+              className="navbar-content"
+            >
+              Testimonials
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Link
+        onClick={closeMenu}
+        activeClass="navbar--active-content"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        to="Contact"
+        className="btn btn-outline-primary"
+      >
+        Contact Me
+      </Link>
     </nav>
   );
 }
