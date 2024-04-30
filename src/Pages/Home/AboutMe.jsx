8c3d3693a411c6efbc4540f42d74d941d7e7d7ea
Lucas Ferraz Martins <lucasuk89@gmail.com>
2024-04-30 16:58:49 +0200

    Update in css and started the AboutMe Function and updated in the index

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section--description">
+            A expressão Lorem ipsum em design gráfico e editoração é um texto
+            padrão em latim utilizado na produção gráfica para preencher os
+            espaços de texto em publicações para testar e ajustar aspectos
+            visuais antes de utilizar conteúdo real
+          </p>
+          <p className="hero--section--description">
+            A expressão Lorem ipsum em design gráfico e editoração é um texto
+            padrão em latim utilizado na produção gráfica para preencher os
+            espaços de texto em publicações para testar e ajustar aspectos
+            visuais antes de utilizar conteúdo real
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <MySkills />
+      <AboutMe />
     </>
   );
 }
