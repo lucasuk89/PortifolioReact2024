fbac8834822d9ca45143d02ac5541738731bb592
Lucas Ferraz Martins <lucasuk89@gmail.com>
2024-05-02 11:22:20 +0200

    Created the form inside of the Contact Me but need to apply the css yet

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -3,6 +3,7 @@ import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
 import MyPortfolio from "../MyPortfolio";
 import Testimonials from "../Testimonials";
+import ContactMe from "../ContactMe";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutMe />
       <MyPortfolio />
       <Testimonials />
+      <ContactMe />
     </>
   );
 }
