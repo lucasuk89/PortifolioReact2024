fd8fd0e763a525788735feb436b4c837596b584b
Lucas Ferraz Martins <lucasuk89@gmail.com>
2024-05-07 11:08:06 +0200

    Introduced some new images on Aboutme section and updated some text on it

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,23 +2,26 @@ export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/about-me.png" alt="About Me" />
+        <img src="./img/iceland.png" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
-          <p className="section--title">About</p>
+          <p className="section--title">..here's a little extra about me</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section--description">
-            A expressão Lorem ipsum em design gráfico e editoração é um texto
-            padrão em latim utilizado na produção gráfica para preencher os
-            espaços de texto em publicações para testar e ajustar aspectos
-            visuais antes de utilizar conteúdo real
+            Hey everyone!
+            <br />
+            <br />
+            I'm a guy who's all about the good stuff in life: traveling, gaming,
+            and kicking a ball around on the field. <br />
+            Whether it's soccer or any other sport, count me in—I'm always up
+            for trying something new.
           </p>
           <p className="hero--section--description">
-            A expressão Lorem ipsum em design gráfico e editoração é um texto
-            padrão em latim utilizado na produção gráfica para preencher os
-            espaços de texto em publicações para testar e ajustar aspectos
-            visuais antes de utilizar conteúdo real
+            I'm someone who loves to interact both in the workplace and beyond.
+            <br />I believe positivity is the key to building meaningful
+            connections between people and creating a more peaceful work
+            environment.
           </p>
         </div>
       </div>
