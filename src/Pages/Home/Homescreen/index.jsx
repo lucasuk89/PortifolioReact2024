108a87ec87bf2922c70603997aa501106ca8f6e9
Lucas Ferraz Martins <lucasuk89@gmail.com>
2024-05-08 12:01:37 +0200

    Started the Design file and function to link with the Figmas content

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -3,6 +3,7 @@ import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
 import MyPortfolio from "../MyPortfolio";
 import Objectives from "../Objectives";
+import Design from "../Design";
 import ContactMe from "../ContactMe";
 import Footer from "../Footer";
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <AboutMe />
       <MyPortfolio />
       <Objectives />
+      <Design />
       <ContactMe />
       <Footer />
     </>
