c565a890fc6e7b411762081f53fa0be4d04511c2
Lucas Ferraz Martins <72750744+lucasuk89@users.noreply.github.com>
2024-05-10 12:03:46 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "lucasuk89@gmail.com",
-    pass: "",
+    pass: "uizwovgvbermejrz",
   },
 });
 
@@ -20,7 +22,7 @@ app.post("/send-email", (req, res) => {
   const { firstName, lastName, email, phoneNumber, topic, message } = req.body;
 
   const mailOptions = {
-    from: "SEU_EMAIL@gmail.come",
+    from: "SEU_EMAIL@gmail.com",
     to: "lucasuk89@gmail.com",
     subject: "Novo contato recebido",
     html: `
@@ -40,6 +42,7 @@ app.post("/send-email", (req, res) => {
     } else {
       console.log("E-mail recebido" + info.response);
       res.status(200).send("E-mail sent with sucess");
+      alert("Your email has been sent");
     }
   });
 });
